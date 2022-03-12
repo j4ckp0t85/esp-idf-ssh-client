f8b2fdb6fa942cd718d85ec143a6b56b9c4b30be
Michele Granato <michele.granato985@gmail.com>
2022-03-12 18:33:53 +0100

    button+diode trigger

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -15,6 +16,7 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
@@ -24,10 +26,18 @@
 #include "esp_spiffs.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "driver/gpio.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+#define GPIO_OUTPUT_IO_0    CONFIG_GPIO_OUTPUT_0
+#define GPIO_OUTPUT_IO_1    CONFIG_GPIO_OUTPUT_1
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+#define GPIO_INPUT_IO_0     CONFIG_GPIO_INPUT_0
+#define GPIO_INPUT_IO_1     CONFIG_GPIO_INPUT_1
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define ESP_INTR_FLAG_DEFAULT 0
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -44,6 +54,8 @@ static int s_retry_num = 0;
 
 EventGroupHandle_t xEventGroup;
 int TASK_FINISH_BIT	= BIT4;
+
+static QueueHandle_t gpio_evt_queue = NULL;
 
 static void event_handler(void* arg, esp_event_base_t event_base,
 								int32_t event_id, void* event_data)
@@ -152,6 +164,56 @@ void wifi_init_sta(void)
 
 void ssh_task(void *pvParameters);
 
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+			gpio_set_level(GPIO_OUTPUT_IO_0, 1);
+			gpio_set_level(GPIO_OUTPUT_IO_1, 1);
+			ESP_LOGI(TAG, "Opening file");
+			FILE* fp = fopen("/spiffs/command.txt", "r");
+			if (fp == NULL) {
+				ESP_LOGE(TAG, "Failed to open file for reading");
+				return;
+			}
+
+			char line[256];
+			int count = 0;
+			while(fgets(line, sizeof(line), fp) != NULL) {
+				int lineLen = strlen(line);
+				line[lineLen-1] = 0;
+				ESP_LOGI(TAG, "line=[%s] lineLen=%d", line, lineLen);
+				if (lineLen == 1) continue;
+				if (line[0] == '#') continue;
+
+				// Execute ssh command
+				xEventGroupClearBits( xEventGroup, TASK_FINISH_BIT );
+				xTaskCreate(&ssh_task, "SSH" + count, 1024*8, (void *) &line, 2, NULL);
+				count = count + 1;
+				// Wit for ssh finish.
+				xEventGroupWaitBits( xEventGroup,
+					TASK_FINISH_BIT,	/* The bits within the event group to wait for. */
+					pdTRUE,				/* HTTP_CLOSE_BIT should be cleared before returning. */
+					pdFALSE,			/* Don't wait for both bits, either bit will do. */
+					portMAX_DELAY);		/* Wait forever. */
+			}
+			fclose(fp);
+			ESP_LOGI(TAG, "SSH all finish");
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+			gpio_set_level(GPIO_OUTPUT_IO_1, 0);
+        }
+    }
+}
+
 void app_main(void)
 {
 	//Initialize NVS
@@ -193,34 +255,50 @@ void app_main(void)
 	xEventGroup = xEventGroupCreate();
 	configASSERT( xEventGroup );
 
-	ESP_LOGI(TAG, "Opening file");
-	FILE* fp = fopen("/spiffs/command.txt", "r");
-	if (fp == NULL) {
-		ESP_LOGE(TAG, "Failed to open file for reading");
-		return;
-	}
+	 //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 
-	char line[256];
-	while(fgets(line, sizeof(line), fp) != NULL) {
-		int lineLen = strlen(line);
-		line[lineLen-1] = 0;
-		ESP_LOGI(TAG, "line=[%s] lineLen=%d", line, lineLen);
-		if (lineLen == 1) continue;
-		if (line[0] == '#') continue;
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
 
-		// Execute ssh command
-		xEventGroupClearBits( xEventGroup, TASK_FINISH_BIT );
-		xTaskCreate(&ssh_task, "SSH", 1024*8, (void *) &line, 2, NULL);
+    //change gpio interrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
-		// Wit for ssh finish.
-		xEventGroupWaitBits( xEventGroup,
-			TASK_FINISH_BIT,	/* The bits within the event group to wait for. */
-			pdTRUE,				/* HTTP_CLOSE_BIT should be cleared before returning. */
-			pdFALSE,			/* Don't wait for both bits, either bit will do. */
-			portMAX_DELAY);		/* Wait forever. */
-	}
-	fclose(fp);
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
-	ESP_LOGI(TAG, "SSH all finish");
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
 
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 }
